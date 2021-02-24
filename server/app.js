0498b11c4ea2d6b596df26ca4cfbc08a86344231
Joseph Richards <jrich1850@westada.org>
2021-02-24 14:08:23 -0700

    initial commit

---
+++ server/app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express()
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+const multer = require("multer")
+const storage = multer.diskStorage({
+    
+})
+//const dao = require('./mysqlDao.js');
+//const dao = require('./sqliteDao.js');
+
+const app = express();
+const port = 3000;
+
+app.listen(3000 || process.env.PORT, () => {
+    console.log("Server on...")
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve('../public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile('index.html', {
+        root: path.resolve('../public')
+    });
+});
+
+app.get('/admin', (req, res)  => {
+    res.status(200).sendFile('admin.html', {
+        root: path.resolve('../public')
+    });
+})
+
+app.get('/getMeme', function (req, res, next) {
+})
+
+
+app.get('/insertRating', (request, response)  => {
+    
+    var ratee = request.query.ratee;
+    var stars = request.query.stars;
+    var comment = request.query.comment;
+    
+    dao.insertRating(ratee, stars, comment);
+
+    response.status(200).send( {});
+})
+
+app.get('/getRatings', (request, response)  => {
+    //TODO: connect to the database, select all ratings, and return them in an HTML table or similar
+    response.status(200).send( 'This is just a test, not real ratings. Sorry.' );
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
